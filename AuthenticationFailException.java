cb3299abfd8692067eb573b3665528606f72c2a7
Olexandr031 <100079943+Olexandr031@users.noreply.github.com>
2022-07-31 19:02:08 +0300

    Delete AuthenticationFailException.java

--- AuthenticationFailException.java
+++
@@ -1,7 +0,0 @@
-package com.example.store.exception;
-
-public class AuthenticationFailException extends IllegalArgumentException{
-    public AuthenticationFailException(String msg) {
-        super(msg);
-    }
-}

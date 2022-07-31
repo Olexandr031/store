26ffdd8b7ad6456955297fd564b352ffdce8f55f
Olexandr031 <100079943+Olexandr031@users.noreply.github.com>
2022-07-31 19:06:58 +0300

    Delete CartItemNotExistException.java

--- CartItemNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.store.exception;
-
-public class CartItemNotExistException extends IllegalArgumentException{
-    public CartItemNotExistException(String msg) {
-        super(msg);
-    }
-}

d4eabe5ea9cd67626faaaf79ea6b65a56187b1e5
Olexandr031 <100079943+Olexandr031@users.noreply.github.com>
2022-07-31 19:11:31 +0300

    Delete ProductNotExistException.java

--- ProductNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.store.exception;
-
-public class ProductNotExistException extends IllegalArgumentException{
-    public ProductNotExistException(String msg) {
-        super(msg);
-    }
-}
